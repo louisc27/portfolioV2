e9230d08dc3694807af2553681b04c17b348a7e2
LouisCunningham <cunningtechgroup@outlook.com>
2023-12-13 17:00:00 -0500

    header complete hero started

---
+++ dist/js/main.js
@@ -0,0 +1,33 @@
+// ADD EVENT LISTENER ON MULITPLE ELEMENTS
+
+const addEventOnElements = function (elements, eventType, callback){
+  for (i = 0, len = elements.length; i < len; i++){
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+// NAVBAR TOGGLE FOR MOBILE
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+
+const toggleNavbar = function(){
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+// ACTIVE HEADER WHEN WINDOW SROLLS DOWN TO 100PX
+
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", function(){
+  if(this.window.scrollY > 100){
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
