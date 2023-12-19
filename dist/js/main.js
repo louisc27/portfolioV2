26e78ad20a47b7ef9884afcd7815d4733c5c4c64
LouisCunningham <cunningtechgroup@outlook.com>
2023-12-19 16:36:13 -0500

    completed site

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,22 @@ window.addEventListener("scroll", function(){
     header.classList.remove("active");
   }
 });
+
+// Scroll Reveal
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function (){
+  for(let i = 0, len = revealElements.length; i < len; i++){
+    if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+for (let i = 0, len = revealDelayElements.length; i <len; i++){
+  revealDelayElements[i].style.transitionDelay = revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
